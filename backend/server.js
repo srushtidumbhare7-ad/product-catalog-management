28816bd8f9d2f9e3d24f39f79b1c9170ea29599b
srushtidumbhare7-ad <srushtidumbhare7@gmail.com>
2026-01-17 23:57:34 +0530

    Create server.js

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+
+//connect to mongodb atlas
+mongoose.connect('mongodb+srv://srushtidumbhare2605_db_user:Eo3ErKL99KiTKvcc@cluster0.cs8eec1.mongodb.net/?appName=Cluster0')
+.then(() => console.log('MongoDB connected successfully'))
+.catch(err => console.log('DB connection error: ', err));
+
+// default route
+app.get('/', (req, res) => {
+res.send('Product Catalog Backend Running...');
+});
+
+const productRoutes = require('./routes/productRoutes');
+app.use('/products', productRoutes);
+
+app.listen(3000, () => {
+console.log('Backend running on port 3000');
+});
